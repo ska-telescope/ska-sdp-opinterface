4923ce532cc180ffaf614940adad3ad42bea666f
Mark Ashdown <maja1@mrao.cam.ac.uk>
2022-05-26 10:12:57 +0100

    REL-104: bumped version to 0.2.3

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -47,9 +47,9 @@ author = "SKA SDP Developers"
 # built documents.
 #
 # The short X.Y.Z version.
-version = "0.2.1"
+version = "0.2.3"
 # The full version, including alpha/beta/rc pre-release tags.
-release = "0.2.1"
+release = "0.2.3"
 
 # -- General configuration ------------------------------------------------
 
